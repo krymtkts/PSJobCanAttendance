0286e4d385dde458adf0fe990f6d48a18ea65fd2
krymtkts <takatoshi.kuriyama@gmail.com>
2023-08-12 15:14:19 +0900

    Refine the publish script.

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,9 @@
 Param (
-    [String]$ApiKey,
-    [ValidateSet('Publish', 'DryRun')]$Mode = 'DryRun'
+    [string]
+    $ApiKey,
+    [string]
+    [ValidateSet('Publish', 'DryRun')]
+    $Mode = 'DryRun'
 )
 
 $ModuleName = Get-ChildItem -File -Path ./ -Recurse -Name '*.psd1' | Split-Path -Parent
@@ -9,22 +12,28 @@ Write-Host "Checking modules under $ArtifactPath"
 
 $report = Invoke-ScriptAnalyzer -Path "$ArtifactPath" -Recurse -Settings PSGallery
 if ($report) {
-    Write-Host "Violation found."
+    Write-Host 'Violation found.'
     $report
     exit
 }
-Write-Host "Check OK."
+Write-Host 'Check OK.'
 
-switch ($Mode) {
-    'Publish' {
-        Write-Host "Publishing module: $ModuleName"
-        Publish-PSResource -Path $ArtifactPath -ApiKey $ApiKey -Verbose
-    }
-    'DryRun' {
-        Write-Host "[DRY-RUN]Publishing module: $ModuleName"
-        Publish-PSResource -Path $ArtifactPath -ApiKey $ApiKey -Verbose -WhatIf
+$Params = @{
+    Path = $ArtifactPath
+    ApiKey = $ApiKey
+    Verbose = $true
+    WhatIf = switch ($Mode) {
+        'Publish' {
+            Write-Host "Publishing module: $ModuleName"
+        }
+        'DryRun' {
+            Write-Host "[DRY-RUN]Publishing module: $ModuleName"
+        }
     }
 }
+
+Publish-PSResource @Params
+
 if ($?) {
     Write-Host 'publishing succeed.'
 }
