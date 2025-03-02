919e12729cb14f1b3768e4a5ac5f0f001bdac1d9
krymtkts <takatoshi.kuriyama@gmail.com>
2025-03-02 20:16:17 +0900

    Adjust PSScriptAnalyzer settings.

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,5 +1,7 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ModuleName')]
+param()
 Properties {
-    $ModuleName = Get-ChildItem -File -Path ./ -Recurse -Name '*.psd1' | Split-Path -Parent
+    $ModuleName = Get-ChildItem -File -Path ./ -Recurse -Name '*.psm1' | Split-Path -Parent
 }
 
 Task default -Depends TestAll
@@ -7,12 +9,12 @@ Task default -Depends TestAll
 Task TestAll -Depends Lint, Test
 
 Task Lint {
-    $warn = Invoke-ScriptAnalyzer -Path "./${ModuleName}" -Settings PSGallery
+    $warn = Invoke-ScriptAnalyzer -Path "./${ModuleName}" -Settings PSScriptAnalyzerSettings.psd1
     if ($warn) {
         $warn
         throw 'Invoke-ScriptAnalyzer for {ModuleName} failed.'
     }
-    $warn = Invoke-ScriptAnalyzer -Path ./*.ps1 -Settings PSGallery
+    $warn = Invoke-ScriptAnalyzer -Path ./*.ps1 -Settings PSScriptAnalyzerSettings.psd1
     if ($warn) {
         $warn
         throw 'Invoke-ScriptAnalyzer for ops scripts failed.'
